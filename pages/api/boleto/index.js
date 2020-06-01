61787c35ef3ece0b2de1e945688aacbc600966a8
JoseLiquez <36653856+JoseLiquez@users.noreply.github.com>
2020-06-01 16:10:25 -0600

    Update index.js

--- pages/api/boleto/index.js
+++ pages/api/boleto/index.js
@@ -1,10 +1,30 @@
 import dbConnect from '../../../utils/dbConnect';
 import Boleto from '../../../models/Boleto';
+import Cors from 'cors'
 
+const cors = Cors({
+    methods: ['GET', 'HEAD'],
+  })
+ 
 dbConnect();
+
+
+function runMiddleware(req, res, fn) {
+    return new Promise((resolve, reject) => {
+      fn(req, res, (result) => {
+        if (result instanceof Error) {
+          return reject(result)
+        }
+  
+        return resolve(result)
+      })
+    })
+  }
 
 export default async (req, res) => {
     const { method } = req;
+
+    await runMiddleware(req, res, cors)
 
     switch(method){
         case 'GET':
